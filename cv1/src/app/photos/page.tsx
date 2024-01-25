5cafe865b2728443d2a8df119ae232f40e4ec030
José Fonte <73178551+josefonte@users.noreply.github.com>
2024-01-25 12:20:32 +0000

    photos done ✅

--- cv1/src/app/photos/page.tsx
+++ cv1/src/app/photos/page.tsx
@@ -1,26 +1,223 @@
-import * as React from "react";
-import Image from "next/image";
-
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 
 import { data } from "@/data/photos";
 
 export default function Photos() {
     return (
         <>
-            <ScrollArea className="w-full whitespace-nowrap rounded-md border snap-x snap-mandatory">
-                <div className="flex w-max space-x-4 p-4 ">
-                    {data.map((artwork, index) => (
-                        <figure key={index} className="shrink-0 ">
-                            <div className="overflow-hidden rounded-md">
+            <ScrollArea className="w-full whitespace-nowrap rounded-md  snap-x snap-mandatory">
+                <div className="flex w-max space-x-4 py-4  ">
+                    <div className="flex flex-col gap-1">
+                        <p className="pl-3 text-lg">BJJ 2023</p>
+                        <div className="w-[440px] h-fit grid auto-rows-[200px] grid-cols-1 gap-4 overflow-hidden shrink-0 mr-8">
+                            <div
+                                key={1}
+                                className="row-span-2 col-span-2 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
                                 <img
-                                    src={artwork.img}
-                                    alt={`Photo by ${index}`}
-                                    className="aspect-[3/4] h-fit w-96 object-cover snap-center snap-always"
+                                    src={data[17].img}
+                                    alt={data[17].alt}
+                                    className="h-full w-full object-cover rounded-xl"
                                 />
                             </div>
-                        </figure>
-                    ))}
+                            <div
+                                key={1}
+                                className="row-span-1 col-span-2 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[18].img}
+                                    alt={data[18].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <p className="pl-3 text-lg">Summer 2023</p>
+                        <div className="w-[900px] h-fit grid auto-rows-[200px] grid-cols-4 gap-4 overflow-hidden shrink-0 mr-8">
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[0].img}
+                                    alt={data[0].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={2}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[1].img}
+                                    alt={data[1].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+
+                            <div
+                                key={1}
+                                className="row-span-2 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[2].img}
+                                    alt={data[2].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1  rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[3].img}
+                                    alt={data[3].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 col-span-2 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[4].img}
+                                    alt={data[4].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[5].img}
+                                    alt={data[5].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[6].img}
+                                    alt={data[6].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[7].img}
+                                    alt={data[7].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 col-span-2 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[8].img}
+                                    alt={data[8].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-1">
+                        <p>London 2023</p>
+                        <div className="w-[650px] h-fit grid auto-rows-[200px] grid-cols-3 gap-4 overflow-hidden shrink-0">
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[9].img}
+                                    alt={data[9].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={2}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[10].img}
+                                    alt={data[10].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-2 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[11].img}
+                                    alt={data[11].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+
+                            <div
+                                key={1}
+                                className="row-span-1  rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[12].img}
+                                    alt={data[12].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[13].img}
+                                    alt={data[13].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[14].img}
+                                    alt={data[14].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[15].img}
+                                    alt={data[15].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                            <div
+                                key={1}
+                                className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 "
+                            >
+                                <img
+                                    src={data[16].img}
+                                    alt={data[16].alt}
+                                    className="h-full w-full object-cover rounded-xl"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="w-[1px] "></div>
                 </div>
                 <ScrollBar orientation="horizontal" />
             </ScrollArea>
