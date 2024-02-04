8a39095d4f4bac51811d891aa5d6fcc1654ad85f
José Fonte <73178551+josefonte@users.noreply.github.com>
2024-02-04 19:42:36 +0000

    speed insights added 🏃‍♂️💨

--- cv1/src/app/layout.tsx
+++ cv1/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -68,6 +69,7 @@ export default function RootLayout({
                     <Toaster />
                 </ThemeProvider>
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
