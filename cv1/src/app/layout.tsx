5f1394e7c9bed745c85e96d901abf2e5e2a0f06e
José Fonte <73178551+josefonte@users.noreply.github.com>
2024-02-04 19:38:52 +0000

    analytics

--- cv1/src/app/layout.tsx
+++ cv1/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -66,6 +67,7 @@ export default function RootLayout({
                     </div>
                     <Toaster />
                 </ThemeProvider>
+                <Analytics />
             </body>
         </html>
     );
