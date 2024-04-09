e78fe8d355bb7bbe17bb91898c34aeebdc8d45ad
José Fonte <73178551+josefonte@users.noreply.github.com>
2024-04-09 23:16:08 +0100

    Update page.tsx

--- cv1/src/app/page.tsx
+++ cv1/src/app/page.tsx
@@ -2,11 +2,10 @@ export default function Home() {
     return (
         <div>
             <div>
-                Passionate maker and lifelong learner, recently graduated with a
-                BSc degree in Software Engineering and now pursing a Masters.
+                Recently graduated with a BSc degree in Software Engineering and now pursing a Masters with main interests in Distributed Systems, AI & ML and Software Develpment.
                 Always seeking new challenges and innovative ways to apply
                 technical expertise. Beyond coding, I enjoy sports, music and
-                podcast, cinema and reading up on cutting‑edge tech trends.
+                podcasts, cinema and keeping up with cutting‑edge tech trends.
             </div>
         </div>
     );
